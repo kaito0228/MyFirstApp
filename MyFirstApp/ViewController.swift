729fab51385d0a72a96ae0bfbcc7d1eed49c35fa
ryo.s <aqua.timez.saiko@gmail.com>
2019-04-22 00:24:14 +0900

    Initial Commit

---
+++ MyFirstApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by ryo shirai on 2019/04/22.
+//  Copyright © 2019 ryo shirai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
